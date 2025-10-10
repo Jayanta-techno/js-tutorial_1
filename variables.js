e6fea620c18a61dfa82ce0890402898bbe6a7a7c
Jayanta-techno <jayanta.20244085@gmail.com>
2025-10-10 11:54:50 +0530

    add

---
+++ variables.js
@@ -0,0 +1,12 @@
+
+const account_Id=144593  //can't be changesd
+let account_email="jayntahistory003@gmail.com"
+var accountPassword="6241"
+accountcity="Bankura"
+console.log(account_Id)
+let accountstate; //by default is undefined
+console.table([account_Id,accountPassword,account_email,accountcity,accountstate])
+/*
+prefer not to use var
+because of issues in functional and block scope
+*/
