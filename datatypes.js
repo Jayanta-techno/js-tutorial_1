711d97b835289cd5845cc77e730b924a69c701de
Jayanta-techno <jayanta.20244085@gmail.com>
2025-10-10 12:33:56 +0530

    datatyoes

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat JS code as newer version
+let name="jayanta"
+let age=19
+let isloggedin=true
+let state;
+// num : 2 ^53
+//bigint
+// string
+/*
+boolean=> true/false
+null=> Standalone value
+undefined =>
+symbol=>unique
+object
+
+*/
+console.log(typeof state) //undefined
+// typeof null -> object
