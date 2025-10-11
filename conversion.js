f160358ecc719b26f95328af516109ec83efa920
Jayanta-techno <jayanta.20244085@gmail.com>
2025-10-11 11:01:22 +0530

    conversion of dtype

---
+++ conversion.js
@@ -0,0 +1,24 @@
+let score=3
+console.log(typeof score)
+let score1="4349"
+console.log(typeof score1)
+let num=Number(score1)
+console.log(num)
+let score2=null
+console.log(typeof score2)
+let num1=Number(score2)
+console.log(num1)
+let score3=undefined
+console.log(typeof score3)
+let num2=Number(score3)
+console.log(num2)
+/*
+"jaya"=>NaN
+true=>1
+false=>0
+if converted in boolean
+"ijfd"=>true
+*/
+let loggedin=0
+let boolean=Boolean(loggedin)
+console.log(boolean)
