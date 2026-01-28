365bd75ed7c9e4d39f0d74991d61cbc38e1afe04
Jayanta-techno <jayanta.20244085@gmail.com>
2026-01-28 18:45:46 +0530

    Project_2(BMI calculaor)

---
+++ project/project_2/backend.js
@@ -0,0 +1,24 @@
+const form=document.querySelector('form')
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+  const height=parseInt(document.querySelector('#height').value);
+  const weight=parseInt(document.querySelector('#weight').value);
+  const results=document.querySelector('#results');
+  if(height==''||height<0||isNaN(height)){
+    results.innerHTML=`please give a valid ${height}`;
+  }else if(weight==" "||weight<0||isNaN(weight)){
+    results.innerHTML=`please give a valid ${weight}`;
+  }else{
+    const bmi=(weight/((height*height)/10000)).toFixed(2);
+    let message="";
+    if(bmi<18.6){
+      message="you are under weight."
+    }else if(bmi<24.9){
+      message="you are normal."
+    }else{
+      message="you are over weight."
+    }
+    results.innerHTML=`<span>your bmi is <strong>${bmi}</strong><br>
+    <span>${message}</span>`
+  }
+})
